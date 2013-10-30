b004b43e9e305b5d9de6fd89b86b90076820b733
Sean Gilbert <segilbert@deloitte.com>
2013-10-30 15:12:53 -0400

    moved scritps/app to root ~/app to show another way to do requirejs wiring. updates to spa mvc, updated to durandal 2.0.

---
+++ src/requirejs.demo.mvc/App/services/logger.js
@@ -0,0 +1,39 @@
+﻿define(['jquery', 'toastr'],
+    function ($, toastr) {
+        var logger = {
+            log: log,
+            logError: logError
+        };
+
+        return logger;
+        
+        function log(message, source) {
+            logIt(message, null, source, true, 'info');
+        }
+        
+        function logInfo(message, data, source, showToast) {
+            logIt(message, data, source, showToast, 'info');
+        }
+
+        function logError(message, data, source, showToast) {
+            logIt(message, data, source, showToast, 'error');
+        }
+
+        function logIt(message, data, source, showToast, toastType) {
+            source = source ? '[' + source + '] ' : '';
+            if (data) {
+                console.log(source, message, data);
+            } else {
+                console.log(source, message);
+            }
+            if (showToast) {
+                if (toastType === 'error') {
+                    toastr.error(message);
+                } else {
+                    toastr.info(message);
+                }
+
+            }
+
+        }
+    });
